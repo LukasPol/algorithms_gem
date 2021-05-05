c4fac8c7d2df3b6c7df09863735d8c9fb5b213bc
Lukas Pol <lukaspaespol@gmail.com>
2021-05-05 07:12:58 -0300

    test: create tests for jump search

---
+++ algorithms_gem/spec/jump_search_spec.rb
@@ -0,0 +1,22 @@
+require 'jump_search'
+
+RSpec.describe 'jump Search' do
+  it "If return element's index in array" do
+    arr = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
+    number = 21
+
+    index = JumpSearch.new(arr, number).search
+
+    expect(index).to eq(8)
+  end
+
+  it "If return -1, if not is in array" do
+    arr = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
+    number = 50
+    len = arr.length - 1
+
+    index = JumpSearch.new(arr, number).search
+
+    expect(index).to eq(-1)
+  end
+end
