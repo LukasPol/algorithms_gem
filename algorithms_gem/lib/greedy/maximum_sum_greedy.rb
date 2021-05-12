c523e03906e6aadf05d0459f2dd8baa1de78d2bb
Lukas Pol <lukaspaespol@gmail.com>
2021-05-12 05:44:31 -0300

    chore(greedy): refactoring maximum sum

--- algorithms_gem/lib/greedy/maximum_sum_greedy.rb
+++ algorithms_gem/lib/greedy/maximum_sum_greedy.rb
@@ -1,5 +1,7 @@
 module Greedy
   class MaximumSumGreedy
+    attr_reader :arr, :k
+
     def initialize(arr, k)
       @arr = arr
       @k = k
@@ -12,26 +14,23 @@ module Greedy
     private
 
     def sum
-      @k.times do
-        min = @arr.max
+      k.times do
+        min = arr.max
 
         index = 0
-        @arr.each_with_index do |item, i|
+        arr.each_with_index do |item, i|
           if item < min
             min = item
             index = i
           end
         end
 
-        next if min == 0
+        break arr if min == 0
 
-        @arr[index] = -@arr[index]
+        arr[index] = -arr[index]
       end
 
-      amount = @arr.sum
-
-      amount
+      arr.sum
     end
-    
   end
 end
